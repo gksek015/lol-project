ebc16285eacc46f23e2bdf347ef9b1a249e9c759
gksek015 <dayoung1490@gmail.com>
2024-12-17 00:45:12 +0900

    add: riotApi.ts

--- src/types/ChampionRotation.ts
+++ src/types/ChampionRotation.ts
@@ -1,5 +1,5 @@
 // 신규 플레이어용 챔피언 로테이션 타입
-export interface NewPlayerChampionRotation {
+export interface ChampionRotation {
     maxNewPlayerLevel: number;
     freeChampionIdsForNewPlayers: number[];
     freeChampionIds: number[];
---
+++ src/utils/riotApi.ts
@@ -0,0 +1,23 @@
+import { ChampionRotation } from "@/types/ChampionRotation";
+import { headers } from "next/headers";
+
+const RIOT_API = "https://kr.api.riotgames.com";
+const RIOT_API_KEY = process.env.RIOT_API_KEY;
+
+export async function getChampionRotation(): Promise<ChampionRotation> {
+    if(!RIOT_API_KEY) {
+        throw new Error("API KEY 오류")
+    }
+
+    const res = await fetch(`${RIOT_API}/lol/platform/v3/champion-rotations`, {
+        headers: {
+            'X-Riot-Token' : RIOT_API_KEY,
+        }
+    })
+
+    if(!res.ok) {
+        throw new Error("로드실패")
+    }
+
+    return res.json();
+}
